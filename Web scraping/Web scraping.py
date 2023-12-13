c0e8d96457d302a9f8a8b08134eb175b69ec6303
Xavier <xsuarezarcia@gmail.com>
2023-12-12 20:54:33 -0500

    terminewebscrappin

--- Web scraping/Web scraping.py
+++ Web scraping/Web scraping.py
@@ -1,30 +1,40 @@
 import csv
 import requests
 from bs4 import BeautifulSoup
-import re
+import json
 
-# Leer el archivo CSV
-with open('./links.csv', 'r') as file:
-      reader = csv.reader(file)
-      # Obtener las URL que aparecen dentro del fichero en la columna correspondiente
-      urls = [row[1] for row in reader]
+# Abrir el archivo json
+with open('base de datos.json') as bdjson:
+    json_data = json.load(bdjson)
 
-#obtener los datos de cada una de las urls y parsear el html
-responses=[]
-for url in urls:
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    responses.append(soup)
+    # Leer el archivo CSV
+    with open('./Web scraping/links.csv', 'r') as file:
+        reader = csv.reader(file)
 
-#crear una lista vacia que almacene el contenido visible de cada uno de los reportes
-text=[]
-#obtener el texto visible de cada uno de los urls
-for content in responses:
-    visible_text= content.get_text()
-    text.append(visible_text)
-print(text)
-#almacenar la información de los reportes en un archivo txt
-with open("reportes.txt","w") as report:
-    for info in text:
-        report.write(info+"./n")
+        #obtener los datos de cada una de las urls y parsear el html
+        responses={}
+        for row in reader:
+            if row[3] in json_data:
+                continue
+            try:
+                response = requests.get(row[1])
+                soup = BeautifulSoup(response.text, 'html.parser')
+            except:
+                continue
+            responses[row[3]] = [
+                {
+                    'Maxima afectacion': 0,
+                    'MW disponibles': 0,
+                    'Demanda del dia': 0,
+                    'MW indisponibles por averias': 0,
+                    'MW en mantenimiento': 0,
+                    'MW limitados en la generacion termica': 0,
+                    'Termoelectricas fuera de servicio': [],
+                    'Termoelectricas en funcionamiento': [],
+                    'Info':soup.get_text()
+                }
+            ]
 
+# Guardar el archivo JSON actualizado
+with open('base de datos.json', 'w') as f:
+    json.dump(json_data, f)
